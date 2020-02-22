c738ec90c0fc3e3cee6b18f5d6c5c62eba8a0ed5
msabet94 <msabet1994@gmail.com>
2020-02-22 13:37:52 -0700

    Added HNR and MCRA code
    Added an m file to calculate frame by frame HNR, average and compare to a threshold to make a decision.
    Added source code from israel Cohens website for mcra. This one still needs alot of work.
    https://israelcohen.com/software/

---
+++ Dev/Src/Bin/HNR.m
@@ -0,0 +1,45 @@
+% initialize parameters
+windowSize  = 96000;
+Fs          = 96000;
+shift       = 125;
+frameStart  = 1;
+frameStop	= windowSize;
+frameIdx    = 1;
+hnrThresh   = 0.3;
+w           = hamming(windowSize);
+
+% read audio signal
+[audioIn,fs]= audioread('STE-063.wav');
+
+% average both channels
+audioIn = mean(audioIn,2);
+
+% high-pass filter the audio to remove LF noise
+hpFilt = designfilt('highpassiir','FilterOrder',8, 'PassbandFrequency',3000,'PassbandRipple',0.2, 'SampleRate',Fs);       
+audioIn = filtfilt(hpFilt,audioIn);
+
+% plot the spectrogram
+spectrogram(audioIn,windowSize,windowSize/2,2048,Fs,'yaxis');
+colormap(spring);
+
+% initialize the output signal
+harmonicSignal = [];
+
+while frameStop < length(audioIn)
+    
+    
+    hnr = harmonicRatio(audioIn(frameStart:frameStop),fs);
+    
+    if hnr > hnrThresh
+        harmonicSignal = [harmonicSignal, audioIn(frameStart:frameStop)]; %#ok<AGROW>
+    end
+    
+    % increment counters
+    frameStart = frameStart + windowSize;
+    frameStop = frameStop + windowSize;
+    frameIdx = frameIdx +1;  
+end
+
+% write out harmonic signal output
+
+audiowrite('HarmonicOutput.wav',Fs)
