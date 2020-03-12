8dea44b40d0f581d7ed3656ed946ed6cc50162d1
Tdrosent <40351112+Tdrosent@users.noreply.github.com>
2020-03-11 22:41:12 -0600

    #6: Added spectrograms to the SNRCalcuation script.   -Now each one of the files will be put through the MCRA algo    -Then the before and after file will be read in, SNR calculated    -The corresponding spectrograms will be generated on a plot and saved off as a .tiff file (I like these better bc no compression)    -Also the results will be captured within a .txt file
    It does not look like we get amazing results for the Training files with actual calls (Average improvement is ~0.0534 dB, but the no voice segment had an improvement of ~2dB)

--- Dev/Src/MatLabFiles/omlsaSNRCalculation.m
+++ Dev/Src/MatLabFiles/omlsaSNRCalculation.m
@@ -1,5 +1,10 @@
 % Utility script to calculate the SNR of a wav file before and after MCRA
 
+%Defining some parameters used to generate Spectrogram
+W = 4096; %window size
+win = window(@blackmanharris,W);
+noverlap = W/2;
+
 %Datapath to the realative wav file location
 datapath1 = char('../../../Data/OMSLA-SNR-Calculation-Files/');
 datapath2 = char('../../../Data/OMSLA-SNR-Calculation-Files/After/');
@@ -12,13 +17,22 @@ FileNames = {'comboCallsTrain063-Before-OMSLA',...
              'trillCallsTrain063-Before-OMSLA',...
              'tsikCallsTrain063-Before-OMSLA'};
          
-         %Experiment numbers to process 
+%Experiment numbers to process 
 FileNamesOutput = {'comboCallsTrain063-After-OMSLA',...
                    'noVoiceCallsTrain062-After-OMSLA',...
                    'peepCallsTrain063-After-OMSLA',...
                    'pheeCallsTrain063-After-OMSLA',...
                    'trillCallsTrain063-After-OMSLA',...
                    'tsikCallsTrain063-After-OMSLA'};
+               
+%Name of calls in order
+CallNames = {'Combo',...
+             'no Voice',...
+             'Peep',...
+             'Phee',...
+             'Trill',...
+             'Tsik',...
+            };
 
 %Initalizing a cell to hold path & wav file names to push down into OMSLA
 List1 = cell(length(FileNames),1);
@@ -37,13 +51,32 @@ end
 fid = fopen(strcat(datapath2,'OMSLA-SNR-Before-and-After.txt'),'w');
 
 for i =1:length(FileNames)
-    [orignal,~] = audioread([List1{i},'.wav']);
+    [orignal,Fs] = audioread([List1{i},'.wav']);
     [After,~]   = audioread([List2{i},'.wav']);
     
     origSNR = snr(orignal);
     AfterSNR = snr(After);
     dif = abs(AfterSNR - origSNR);
     fprintf(fid,"%s SNR: %f,\n %s SNR: %f,\n improvement: %f\n\n", List1{i}, origSNR, List2{i},AfterSNR,dif);
+    
+    plots=figure();
+    set(0,'DefaultFigureVisible','off');
+    subplot(2,1,1);
+    [~,freq_original,time_original,psd_original]=spectrogram(orignal,win,noverlap,W,Fs,'yaxis');
+    surf(time_original,freq_original,10*log10(psd_original),'edgecolor','none');
+    axis tight; view(0,90);
+    title({strcat(CallNames{i},'-063'),strcat('Original Call: SNR =', num2str(origSNR),'dB')});
+    xlabel('Time (s)'); ylabel('Frequency (Hz)'); h = colorbar; ylabel(h, 'dB'); colormap winter;
+    
+    subplot(2,1,2);
+    [~,freq_After,time_After,psd_After]=spectrogram(After,win,noverlap,W,Fs,'yaxis');
+    surf(time_After,freq_After,10*log10(psd_After),'edgecolor','none');
+    axis tight; view(0,90);
+    title({strcat('After OMLSA: SNR =', num2str(AfterSNR),'dB'),strcat("Improvement = ", num2str(dif),'dB')});
+    xlabel('Time (s)'); ylabel('Frequency (Hz)'); h = colorbar; ylabel(h, 'dB'); colormap winter;
+    
+    saveas(plots,strcat(List2{i},'.tiff'));
+
 end
 fclose(fid);
 
