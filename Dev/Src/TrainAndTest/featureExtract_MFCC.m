cabe9096d37093f48e62920971e1f92840b4cd0f
Tdrosent <40351112+Tdrosent@users.noreply.github.com>
2020-04-04 20:02:38 -0600

    Updated the feature extract to handle the testing data.

--- Dev/Src/TrainAndTest/featureExtract_MFCC.m
+++ Dev/Src/TrainAndTest/featureExtract_MFCC.m
@@ -1,23 +1,17 @@
 % feature exctraction
 function featureExtract_MFCC()
-dataPath = char('../../../Data/MasterTraining');
-addpath(dataPath)
+dataPathTrain = char('../../../Data/MasterTraining');
+dataPathTest = char('../../../Data/MasterTesting');
+addpath(dataPathTrain)
+addpath(dataPathTest)
 
 fs = 96000;
-%CallNames = {'twitterCalls','pheeCalls','trillCalls'};
 CallNames      = {'peep','peepString','phee','trill', 'tsik','tsikString','twitter'};
 
-% Twitter1 = audioread(['NEEDED/Training/twitterCallsTrain.wav']);
-% Twitter2 = audioread(['NEEDED/Training/twitterCallsTrain2.wav']);
-% PheeCalls1 = audioread(['NEEDED/Training/pheeCallsTrain.wav']);
-% PheeCalls2 = audioread(['NEEDED/Training/pheeCallsTrain2.wav']);
-% TrillCalls1 = audioread(['NEEDED/Training/trillCallsTrain.wav']);
-% TrillCalls2 = audioread(['NEEDED/Training/trillCallsTrain2.wav']);
-% FullData = [Twitter1;Twitter2;PheeCalls1;PheeCalls2;TrillCalls1;TrillCalls2];
 FullData = [];
 for i = 1:length(CallNames)
-    FileName = strcat(dataPath,'\',CallNames{i},'CallsTrainMaster.wav');
-    [y,~]= audioread(FileName);
+    FileNameTrain = strcat(dataPathTrain,'\',CallNames{i},'CallsTrainMaster.wav');
+    [y,~]= audioread(FileNameTrain);
     FullData = [FullData ; y];
 end
 
@@ -30,8 +24,7 @@ numCoeffsList = 12;
 for k = 1:length(numCoeffsList)
     for j = 1:length(windowSizeList)
         TrainingFolderName = strcat('../../../Data/Features/Training/MFCC',num2str(j),num2str(k));
-        %!!!! Will use later
-%         TestingFolderName = strcat('../../../Data/Features/Testing/MFCC',num2str(j),num2str(k));
+        TestingFolderName  = strcat('../../../Data/Features/Testing/MFCC',num2str(j),num2str(k));
 
         %First extract the features from the Full Data, Z-Score
         %normailization must be done on all of the data not on a call by
@@ -44,52 +37,41 @@ for k = 1:length(numCoeffsList)
         sigma = std(FullFeatures);
         NormalizationFactor.mu = mu;
         NormalizationFactor.sigma=sigma;
+        
         if ~exist(TrainingFolderName,'dir')
                 mkdir(TrainingFolderName);
         end
-        save(strcat(TrainingFolderName,'/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_NormalizationFactors.mat'),'NormalizationFactor')
+        
+        if ~exist(TestingFolderName,'dir')
+            mkdir(TestingFolderName);
+        end
+        
+        save(strcat(TrainingFolderName,'/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_NormalizationFactors.mat'),'NormalizationFactor');
+        
         for i = 1:length(CallNames)
 
-            FileName = strcat(dataPath,'\',CallNames{i},'CallsTrainMaster.wav');
+            FileNameTrain = strcat(dataPathTrain,'\',CallNames{i},'CallsTrainMaster.wav');
             
-            TrainwaveIn = audioread(FileName);
+            FileNameTest = strcat(dataPathTest,'\',CallNames{i},'CallsTestMaster.wav');
+
+            TrainwaveIn = audioread(FileNameTrain);
             
-            % !!!!!! Still need to create testing wave files
-%             TestwaveIn1 = audioread(['NEEDED/Testing/' waveFile 'Test.wav']);
-%             TestwaveIn2 = audioread(['NEEDED/Testing/' waveFile 'Test2.wav']);
-%             TestwaveIn = [TestwaveIn1;TestwaveIn2];
+            TestwaveIn = audioread(FileNameTest);
             
             [coeffsTrain,deltaTrain,deltaDeltaTrain] = mfcc(TrainwaveIn,fs,'WindowLength',wlist(j),'OverlapLength',l(j),'NumCoeffs',numCoeffsList(k));
-%             [coeffsTest,deltaTest,deltaDeltaTest] = mfcc(TestwaveIn,fs,'WindowLength',wlist(j),'OverlapLength',l(j),'NumCoeffs',numCoeffsList(k));
+            [coeffsTest,deltaTest,deltaDeltaTest] = mfcc(TestwaveIn,fs,'WindowLength',wlist(j),'OverlapLength',l(j),'NumCoeffs',numCoeffsList(k));
             
             TrainFeatures = [coeffsTrain,deltaTrain,deltaDeltaTrain];
-%             TestFeatures = [coeffsTest,deltaTest,deltaDeltaTest];
+            TestFeatures = [coeffsTest,deltaTest,deltaDeltaTest];
             
             % normalize the data
             TrainNormFeatures = ((TrainFeatures-mu))./sigma;
-%             TestNormFeatures = ((TestFeatures-mu))./sigma;
+            TestNormFeatures = ((TestFeatures-mu))./sigma;
 
-%             if ~exist(strcat(TrainingFolderName,'/Normalized/'),'dir')
-%                 mkdir(strcat(TrainingFolderName,'/Normalized/'));
-%             end
-            %I dont think we need to save the regular features right now
-%             if ~exist(strcat('NEEDED/ModelsTrain/MODEL',num2str(j),num2str(k),'/Regular/'),'dir')
-%                 mkdir(strcat('NEEDED/ModelsTrain/MODEL',num2str(j),num2str(k),'/Regular/'));
-%             end
-            
-%             if ~exist(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Normalized/'),'dir')
-%                 mkdir(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Normalized/'));
-%             end
-%             if ~exist(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Regular/'),'dir')
-%                 mkdir(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Regular/'));
-%             end
 
-            save(strcat(TrainingFolderName,'/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_', CallNames{i}, '.mat'),'TrainNormFeatures')
-%             save(strcat('NEEDED/ModelsTrain/MODEL',num2str(j),num2str(k),'/Regular/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_FULL_', waveFile, '.mat'),'TrainNormFeatures')
-            
+            save(strcat(TrainingFolderName,'/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_', CallNames{i}, '.mat'),'TrainNormFeatures')        
 
-%             save(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Normalized/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_FULL_', waveFile, '.mat'),'TestNormFeatures')
-%             save(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Regular/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_FULL_', waveFile, '.mat'),'TestNormFeatures')
+            save(strcat(TrainingFolderName,'/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_FULL_', CallNames{i}, '.mat'),'TestNormFeatures')
         end
     end
 end
