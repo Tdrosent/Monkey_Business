147368cec07a128c0c602ed11a9f4ce6e1e07c3d
msabet94 <40343175+msabet94@users.noreply.github.com>
2020-04-14 14:59:18 -0700

    Updated script for HPS extract. This does HPS separately on each call then combines the features, rather than running HPS on the full file. This bypasses the feature length issue. We will need to do something similiar for HNR extract. The only thing left for HPS is to fix the normalization, since it will have to be done after grouping all the features for the full set.

--- Dev/Src/TrainAndTest/featureExtract_HPS.m
+++ Dev/Src/TrainAndTest/featureExtract_HPS.m
@@ -10,15 +10,12 @@ fs = 96000;
 CallNames      = {'peep','peepString','phee','trill', 'tsik','tsikString','twitter'};
 
 
-FullData = [];
-for i = 1:length(CallNames)
-    FileName = strcat(dataPath,'\',CallNames{i},'CallsTrainMaster.wav');
-    [y,~]= audioread(FileName);
-    FullData = [FullData ; y];
-end
+FullFeatures = [];
 
 windowSizeList = [ 0.02, 0.05, 0.1, 0.2, 1];
+
 wlist = round(windowSizeList *fs);
+
 mstimeList = windowSizeList * 10^3;
 
 for j = 1:length(windowSizeList)
@@ -27,56 +24,43 @@ for j = 1:length(windowSizeList)
     %First extract the features from the Full Data, Z-Score
     %normailization must be done on all of the data not on a call by
     %call basis
-    FullFeatures = HPSFeatures(FullData,wlist(j),3);
     
     %Dynamically computing z-score Normalization factors
-    mu = mean(FullFeatures);
-    sigma = std(FullFeatures);
-    NormalizationFactor.mu = mu;
-    NormalizationFactor.sigma=sigma;
+    
+    
     if ~exist(TrainingFolderName,'dir')
         mkdir(TrainingFolderName);
     end
-    save(strcat(TrainingFolderName,'/Features_','HPS_',num2str(mstimeList(j)),'ms_NormalizationFactors.mat'),'NormalizationFactor')
+    
     for i = 1:length(CallNames)
         
         FileName = strcat(dataPath,'\',CallNames{i},'CallsTrainMaster.wav');
         
         TrainwaveIn = audioread(FileName);
         
-        % !!!!!! Still need to create testing wave files
-        %             TestwaveIn1 = audioread(['NEEDED/Testing/' waveFile 'Test.wav']);
-        %             TestwaveIn2 = audioread(['NEEDED/Testing/' waveFile 'Test2.wav']);
-        %             TestwaveIn = [TestwaveIn1;TestwaveIn2];
+        TrainFeatures{:,:,i} = HPSFeatures(TrainwaveIn,wlist(j),3);
         
-        TrainFeatures = HPSFeatures(TrainwaveIn,wlist(j),3);
+        currentFeature =  TrainFeatures(:,i);
         
-        % normalize the data
-        TrainNormFeatures = ((TrainFeatures-mu))./sigma;
-        %             TestNormFeatures = ((TestFeatures-mu))./sigma;
+        save(strcat(TrainingFolderName,'/Features_','HPS_',num2str(mstimeList(j)),'ms_', CallNames{i}, '.mat'),'currentFeature')
         
-        %             if ~exist(strcat(TrainingFolderName,'/Normalized/'),'dir')
-        %                 mkdir(strcat(TrainingFolderName,'/Normalized/'));
-        %             end
-        %I dont think we need to save the regular features right now
-        %             if ~exist(strcat('NEEDED/ModelsTrain/MODEL',num2str(j),num2str(k),'/Regular/'),'dir')
-        %                 mkdir(strcat('NEEDED/ModelsTrain/MODEL',num2str(j),num2str(k),'/Regular/'));
-        %             end
+        FullFeatures = [FullFeatures;TrainFeatures{:,:,i}];
         
-        %             if ~exist(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Normalized/'),'dir')
-        %                 mkdir(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Normalized/'));
-        %             end
-        %             if ~exist(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Regular/'),'dir')
-        %                 mkdir(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Regular/'));
-        %             end
+    end
+    mu = mean(FullFeatures);
+    sigma = std(FullFeatures);
+    NormalizationFactor.mu = mu;
+    NormalizationFactor.sigma=sigma;
+    
+    %   normalize the data
+    TrainNormFeatures = ((TrainFeatures-mu))./sigma;
+    
+    save(strcat(TrainingFolderName,'/Features_','HPS_',num2str(mstimeList(j)),'ms_NormalizationFactors.mat'),'NormalizationFactor')
+    for i = 1:length(CallNames)
         
-        save(strcat(TrainingFolderName,'/Features_','HPS_',num2str(mstimeList(j)),'ms_', CallNames{i}, '.mat'),'TrainNormFeatures')
-        %             save(strcat('NEEDED/ModelsTrain/MODEL',num2str(j),num2str(k),'/Regular/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_FULL_', waveFile, '.mat'),'TrainNormFeatures')
+        currentFeature =  TrainFeatures{:,:,i};
         
-        
-        %             save(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Normalized/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_FULL_', waveFile, '.mat'),'TestNormFeatures')
-        %             save(strcat('NEEDED/ModelsTest/MODEL',num2str(j),num2str(k),'/Regular/Features_',num2str(numCoeffsList(k)),'mfccs_',num2str(mstimeList(j)),'ms_FULL_', waveFile, '.mat'),'TestNormFeatures')
-        
+        save(strcat(TrainingFolderName,'/Normalized_Features_','HPS_',num2str(mstimeList(j)),'ms_', CallNames{i}, '.mat'),'currentFeature')
     end
 end
 
