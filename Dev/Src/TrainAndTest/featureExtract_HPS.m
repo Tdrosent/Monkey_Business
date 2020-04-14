d07c5095e2214bde06c78f65aee25f5521b62270
Tdrosent <40351112+Tdrosent@users.noreply.github.com>
2020-04-14 17:43:15 -0600

    Updated featureExtract_HPS.m to function properly.
    The flow of this feature extraction is as follows:
    
    1) Read in audio file
    2) Run this data through the HPS feature extraction function
    3) Save this data off as a .mat file to be normalized later
    4) Place data into a fullDataSet matrix
    5) Once all of the files have been read in and fullDataSet has been built up we will then compute mu and sigma
    6) Load in non-normalized .mat files and normalize them
    7) Save the data off to train with
    8) Rinse and repeat for all window sizes. 
    
    I will attach the .wav files I was using to debug this in the email that is sent out. LMK if there are any issues with this.

--- Dev/Src/TrainAndTest/featureExtract_HPS.m
+++ Dev/Src/TrainAndTest/featureExtract_HPS.m
@@ -8,9 +8,8 @@ addpath(dataPath)
 fs = 96000;
 %CallNames = {'twitterCalls','pheeCalls','trillCalls'};
 CallNames      = {'peep','peepString','phee','trill', 'tsik','tsikString','twitter'};
-
-
-FullFeatures = [];
+% I will send these files via email in case you guys need them
+% CallNames      = {'DebugFile1','DebugFile2'};
 
 windowSizeList = [ 0.02, 0.05, 0.1, 0.2, 1];
 
@@ -19,49 +18,60 @@ wlist = round(windowSizeList *fs);
 mstimeList = windowSizeList * 10^3;
 
 for j = 1:length(windowSizeList)
-    TrainingFolderName = strcat('../../../Data/Features/Training/HPS',num2str(j));
+    TrainingFolderNameNotNormalized = strcat('../../../Data/Features/Training/HPS/NotNormalized',num2str(j));
+    TrainingFolderNormalized = strcat('../../../Data/Features/Training/HPS/Normalized',num2str(j));
     
-    %First extract the features from the Full Data, Z-Score
-    %normailization must be done on all of the data not on a call by
-    %call basis
-    
-    %Dynamically computing z-score Normalization factors
-    
-    
-    if ~exist(TrainingFolderName,'dir')
-        mkdir(TrainingFolderName);
+    %Need to clear matrix between each window size
+    FullFeatures = [];
+
+    %Create directories needed to place data
+    if ~exist(TrainingFolderNameNotNormalized,'dir')
+        mkdir(TrainingFolderNameNotNormalized);
     end
     
+    if ~exist(TrainingFolderNormalized,'dir')
+        mkdir(TrainingFolderNormalized);
+    end
+    
+    %This is the main loop it will go through each call and extract all of
+    %the HPS features. It will build up a large full data set to compute
+    %the z-score.
+    %On each pass it will save the features of each call in a
+    %non-normalized .mat file that will be loaded in later and normalized
     for i = 1:length(CallNames)
-        
+        TrainFeatures = [];
         FileName = strcat(dataPath,'\',CallNames{i},'CallsTrainMaster.wav');
+        %FileName = strcat(dataPath,'\',CallNames{i},'.wav');
         
         TrainwaveIn = audioread(FileName);
         
-        TrainFeatures{:,:,i} = HPSFeatures(TrainwaveIn,wlist(j),3);
+        TrainFeatures = HPSFeatures(TrainwaveIn,wlist(j),3);
         
-        currentFeature =  TrainFeatures(:,i);
+        %Saving HPS results off as not normalized, we can read in these
+        %features and normalize them after we run through everything.
+        save(strcat(TrainingFolderNameNotNormalized,'/Features_','HPS_',num2str(mstimeList(j)),'ms_', CallNames{i}, 'Not_Normalized.mat'),'TrainFeatures')
         
-        save(strcat(TrainingFolderName,'/Features_','HPS_',num2str(mstimeList(j)),'ms_', CallNames{i}, '.mat'),'currentFeature')
-        
-        FullFeatures = [FullFeatures;TrainFeatures{:,:,i}];
+        %Building up full feature set to compute normalization factors
+        FullFeatures = [FullFeatures;TrainFeatures];
         
     end
+    %Dynamically computing z-score Normalization factors
     mu = mean(FullFeatures);
     sigma = std(FullFeatures);
     NormalizationFactor.mu = mu;
     NormalizationFactor.sigma=sigma;
     
-    %   normalize the data
-    TrainNormFeatures = ((TrainFeatures-mu))./sigma;
+    %Saving Normalization factors in normalized folder
+    save(strcat(TrainingFolderNormalized,'/Features_','HPS_',num2str(mstimeList(j)),'ms_NormalizationFactors.mat'),'NormalizationFactor')
     
-    save(strcat(TrainingFolderName,'/Features_','HPS_',num2str(mstimeList(j)),'ms_NormalizationFactors.mat'),'NormalizationFactor')
     for i = 1:length(CallNames)
-        
-        currentFeature =  TrainFeatures{:,:,i};
-        
-        save(strcat(TrainingFolderName,'/Normalized_Features_','HPS_',num2str(mstimeList(j)),'ms_', CallNames{i}, '.mat'),'currentFeature')
+        CurrentMat = load(strcat(TrainingFolderNameNotNormalized,'/Features_','HPS_',num2str(mstimeList(j)),'ms_', CallNames{i}, 'Not_Normalized.mat'));
+        CurrentFeatures = CurrentMat.TrainFeatures;
+        %   normalize the data
+        TrainNormFeatures = ((CurrentFeatures-mu))./sigma;
+        save(strcat(TrainingFolderNormalized,'/Normalized_Features_','HPS_',num2str(mstimeList(j)),'ms_', CallNames{i}, '.mat'),'TrainNormFeatures')
     end
+    
 end
 
 
